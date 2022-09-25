410e0e1506ef3310f1b2a87fd98f52fbfdbef1d4
Alexandr-Mayyura <sonickill@yandex.ru>
2022-09-25 14:32:28 +0300

    create view and slides

--- MadColorSlider/ViewController.swift
+++ MadColorSlider/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redColorNumber: UILabel!
+    @IBOutlet var greenColorNumber: UILabel!
+    @IBOutlet var blueColorNumber: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupSliders()
+        colorView.layer.cornerRadius = 12
+        colorView.backgroundColor = .black
     }
 
-
+    @IBAction func redSliderAction() {
+        redColorNumber.text = "\(String(format: "%.2f", redSlider.value))  "
+        changeColor()
+    }
+        
+    @IBAction func greenSliderAction() {
+        greenColorNumber.text = "\(String(format: "%.2f", greenSlider.value))  "
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueColorNumber.text = "\(String(format: "%.2f", blueSlider.value))  "
+        changeColor()
+    }
+    
+    private func setupSliders() {
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
 }
 
